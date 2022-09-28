81e6e0cf717c24d5e649193600b57ea792b10950
Drac245 <dmezamestra@gmail.com>
2022-09-28 10:10:52 -0500

    updat

--- venn.py
+++ venn.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import pyplot as plt
 from matplotlib_venn import venn2
 
-venn2(subsets = (30, 110, 20), set_labels = ('grupo 1', 'grupo 2'), set-colors=('deeppink', 'lawngreen'), alpha=0.9)
+venn2(subsets = (30, 20, 110), set_labels = ('grupo 1', 'grupo 2'), set_colors=('deeppink', 'lawngreen'), alpha=0.9)
 plt.title("Diagrama de Venn")
 plt.show()
